55fed762918d7a40d681ffcc2e50523feff0e4f6
Lucas Augusto <lucas.augusto5061@gmail.com>
2020-04-05 15:27:54 -0300

    add plugin for documentation api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+/* eslint-disable global-require */
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
 
 const { validate } = require('./helpers/validators');
 const routes = require('./routes');
+const Package = require('../package');
 
 const init = async () => {
   const server = Hapi.Server({
@@ -11,9 +16,22 @@ const init = async () => {
     port: process.env.APP_PORT,
     routes: { cors: true },
   });
+  const swaggerOptions = {
+    info: {
+      title: 'Test API Documentation',
+      version: Package.version,
+    },
+  };
 
-  // eslint-disable-next-line global-require
-  await server.register(require('@hapi/basic'));
+  await server.register([
+    require('@hapi/basic'),
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions,
+    },
+  ]);
   server.auth.strategy('simple', 'basic', { validate });
 
   routes(server);
--- src/routes/modules/garcon.route.js
+++ src/routes/modules/garcon.route.js
@@ -6,6 +6,9 @@ const garcons = (server) => {
     path: '/garcons',
     options: {
       auth: 'simple',
+      description: 'Get Garçons',
+      notes: 'Returns a list of garçons',
+      tags: ['api'],
     },
     handler: async () => {
       return db.garcons().all();
@@ -17,6 +20,9 @@ const garcons = (server) => {
     path: '/garcons/{id}',
     options: {
       auth: 'simple',
+      description: 'Get Garçon',
+      notes: 'Returns a garçon of passed id for path',
+      tags: ['api'],
     },
     handler: async (request) => {
       const { id } = request.params;
@@ -30,6 +36,9 @@ const garcons = (server) => {
     path: '/garcons',
     options: {
       auth: 'simple',
+      description: 'Save Garçon',
+      notes: 'Save garçon with values of payload',
+      tags: ['api'],
     },
     handler: async (request) => {
       const user = db.garcons().save(request.payload.user);
@@ -42,6 +51,9 @@ const garcons = (server) => {
     path: '/garcons',
     options: {
       auth: 'simple',
+      description: 'Update Garçon',
+      notes: 'Update garçon with values passed of payload',
+      tags: ['api'],
     },
     handler: (request) => {
       const user = db.garcons().update(request.payload.user);
@@ -55,6 +67,9 @@ const garcons = (server) => {
     path: '/garcons',
     options: {
       auth: 'simple',
+      description: 'Delete Garçon',
+      notes: 'Delete Garçon of id passed for payload',
+      tags: ['api'],
     },
     handler: (request) => {
       const user = db.garcons().delete(request.payload.user);
